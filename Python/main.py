efa7405115bf09ab62e426c3cd9356e37f2a0b97
Dario Emanuel Diaz <dariodiaz91498@gmail.com>
2024-02-16 18:20:34 -0300

    16/2

---
+++ Python/main.py
@@ -0,0 +1,5 @@
+vector = archivo = lista = None
+print(f"el valor del vector es: {vector}")
+print(f"el valor del archivo es: {archivo}")
+print(f"el valor de la lista es: {lista}")
+
--- main.py
+++
@@ -1,9 +0,0 @@
-print(f"Hola que tal, buen dia")
-x = "a"
-for i in range(10):
-    if i < 10:
-        x += "a"
-    
-print(f"La concatenacion es {x}")
-
-print(f"Fin del programa")
